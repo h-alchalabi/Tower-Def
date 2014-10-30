a95f5ec103424cb6adf1e09e7eb51a5a8f017934
Awais Ali <awais1994@hotmail.com>
2014-10-30 12:13:25 -0400

    Added Entity .cpp and .h files. CollisionHandler should no longer give out errors.

---
+++ COMP345_Final_Project/COMP345_Final_Project/Entity.cpp
@@ -0,0 +1,22 @@
+#include "Entity.h"
+#include <SFML\Graphics\Rect.hpp>
+#include <string>
+
+Entity::Entity(){
+	// do nothing
+}
+Entity::Entity(std::string path){
+	entityTexture.loadFromFile(path);
+	entitySprite.setTexture(entityTexture);
+}
+void Entity::handleCollision(Entity& e){
+	// function to be overrriden in child classes. (Critters and Towers and Projectiles)
+}
+// Returns the pointer to the Entity's hitbox.
+sf::Rect<float>* Entity::getHitbox(){
+	sf::Rect<float>* hitbox = new sf::Rect<float>(this->entitySprite.getPosition().x, this->entitySprite.getPosition().y, this->entityTexture.getSize().x, this->entityTexture.getSize().y);
+	return hitbox;
+}
+sf::Sprite Entity::getSprite(){
+	return this->entitySprite;
+}
---
+++ COMP345_Final_Project/COMP345_Final_Project/Entity.h
@@ -0,0 +1,20 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <SFML\Graphics.hpp>
+#include <SFML\Graphics\Rect.hpp>
+#include <string>
+class Entity{
+public:
+	Entity();
+	Entity(std::string path);
+	void handleCollision(Entity& e);
+	sf::Rect<float>* getHitbox();
+	sf::Sprite getSprite();
+private:
+	std::string entityPath;
+	sf::Texture entityTexture;
+	sf::Sprite entitySprite;
+};
+
+#endif // ENTITY_H
