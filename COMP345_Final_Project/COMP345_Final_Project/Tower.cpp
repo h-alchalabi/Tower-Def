0d50ecd9c139cc3f9d1b7e56855aede98c6e6d26
Amanda <amanda.my.tom@gmail.com>
2014-11-04 02:01:58 -0500

    Adding the Tower.cpp file

---
+++ COMP345_Final_Project/COMP345_Final_Project/Tower.cpp
@@ -0,0 +1,74 @@
+#include "Tower.h"
+
+Tower::Tower(int type){
+
+	//Average Tower
+	if (type == 0)
+	{
+		lvl = 1;
+		buyingCost = 150;
+		resellVal = 100;
+		range = 3;
+		atk = 30;
+		status = NONE;
+		fireRate = 2;
+		upgradeCost = 45;
+	}
+
+	//Special Type of Tower
+	if (type == 1)
+	{
+		lvl = 1;
+		buyingCost = 175;
+		resellVal = 125;
+		range = 6;
+		atk = 10;
+		status = SLOW;
+		fireRate = 1;
+		upgradeCost = 55;
+	}
+}
+
+Tower::~Tower(){}
+
+int Tower::getLvl(){
+	return lvl;
+}
+
+int Tower::getType(){
+	return type;
+}
+
+int Tower::getBuyCost(){
+	return buyingCost;
+}
+
+int Tower::getResellVal(){
+	return resellVal;
+}
+
+float Tower::getRange(){
+	return range;
+}
+
+int Tower::getAtk(){
+	return atk;
+}
+
+float Tower::getRate(){
+	return fireRate;
+}
+
+int Tower::getUpgradeCost(){
+	return upgradeCost;
+}
+
+void Tower::setUpgradeStats()
+{
+	++lvl;
+	resellVal += 15;
+	++range;
+	atk += 5;
+	++fireRate;
+
+}
