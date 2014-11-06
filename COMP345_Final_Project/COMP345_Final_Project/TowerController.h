4c22fe55dd22c11c829cc050b065208db8bf9421
Amanda <amanda.my.tom@gmail.com>
2014-11-05 19:04:43 -0500

    Changed TowerController.h addTower

--- COMP345_Final_Project/COMP345_Final_Project/TowerController.h
+++ COMP345_Final_Project/COMP345_Final_Project/TowerController.h
@@ -9,7 +9,7 @@ public:
 	// constructor initialize the vector
 	TowerController();
 	// create and a tower to the list.
-	void addTower(int type, int level, int x, int y);
+	void addTower(int type, int x, int y);
 	// remove a tower from the list, trim list to proper size.
 	void removeTower(Tower* tower);
 	// return the pointer to the tower at the given index.
