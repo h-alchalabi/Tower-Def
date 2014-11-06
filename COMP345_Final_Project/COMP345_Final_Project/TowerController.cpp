c464b09aa0452b9be1860655704a0d616d6783e5
Amanda <amanda.my.tom@gmail.com>
2014-11-06 01:50:54 -0500

    Added cpp for TowerController, and added upgrade method in h file

---
+++ COMP345_Final_Project/COMP345_Final_Project/TowerController.cpp
@@ -0,0 +1,44 @@
+#include "TowerController.h"
+#include "Tower.h"
+
+#include <vector>
+
+Tower* tower;
+vector<Tower>::iterator i;
+int input;
+int index;
+
+void TowerController::addTower(int type, int posX, int posY){
+	tower = new Tower(type);
+	//Need to tell the tower where it is placed on the map
+	towerList.push_back(*tower);
+	cout << "You purchased a " << tower->getType() << endl;
+};
+
+void TowerController::removeTower(Tower* tower){
+	cout << "Current number of towers owned: " << towerList.size() << endl;
+	index = 0;
+	for (i = towerList.begin(); i != towerList.end(); ++i){
+		cout << "Index " << index << "\tType: " << i->getType() <<
+			"\tResell value: " << i->getResellVal() << endl;
+		++index;
+	}
+	cout << "\nTo sell a tower, input its index." << endl;
+	cin >> input;
+
+	towerList.erase(towerList.begin() + input);
+};
+
+void TowerController::upgradeTower(Tower* tower){
+	cout << "Current number of towers owned: " << towerList.size() << endl;
+	index = 0;
+	for (i = towerList.begin(); i != towerList.end(); ++i){
+		cout << "Index " << index << "\tType: " << i->getType() <<
+			"\tResell value: " << i->getResellVal() << endl;
+		++index;
+	}
+	cout << "\nTo upgrade a tower, input its index." << endl;
+	cin >> input;
+
+	towerList.at(index).setUpgradeStats();
+}
--- COMP345_Final_Project/COMP345_Final_Project/TowerController.h
+++ COMP345_Final_Project/COMP345_Final_Project/TowerController.h
@@ -12,6 +12,8 @@ public:
 	void addTower(int type, int x, int y);
 	// remove a tower from the list, trim list to proper size.
 	void removeTower(Tower* tower);
+	// upgrade a tower from the list
+	void upgradeTower(Tower* tower);
 	// return the pointer to the tower at the given index.
 	Tower* getTower(int i);
 	// return the list of towers.
