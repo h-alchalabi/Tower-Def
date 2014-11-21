e9fa5d5159dbc01de8a4809be4a6bb5940c533ad
Stanley <stanleyluu@gmail.com>
2014-11-21 15:37:57 -0500

    Tower class

--- COMP_345_SFML/COMP_345_SFML/Tower.cpp
+++ COMP_345_SFML/COMP_345_SFML/Tower.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
-Tower::Tower() : Entity(GameConstants::TOWER_IMAGE_NAME){
+Tower::Tower() : Entity(GameConstants::TOWER_IMAGE_NAME) {}
 
+string Tower::to_string() {
+	stringstream ss;
+	ss << "Tower:\n---------------------------------------" << endl;
+	ss << "Level:\t\t\t" << getLevel() << endl;
+	ss << "Base Price:\t\t" << getBasePrice() << endl;
+	ss << "Sell Price:\t\t" << getSellPrice() << endl;
+	ss << "Upgrade Price:\t\t" << getUpgradePrice() << endl;
+	ss << "Damage:\t\t\t" << getDamage() << endl;
+	ss << "Range:\t\t\t" << getRange() << endl;
+	ss << "Fire Rate:\t\t" << getFireRate() << endl;
+	string str = ss.str();
+	return str;
 }
