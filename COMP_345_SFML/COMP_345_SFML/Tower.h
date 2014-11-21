7452cb785eaac02616779a47c378ad2dc443ecac
Awais Ali <awais1994@hotmail.com>
2014-11-21 14:48:23 -0500

    Updated Tower

--- COMP_345_SFML/COMP_345_SFML/Tower.h
+++ COMP_345_SFML/COMP_345_SFML/Tower.h
@@ -1,8 +1,35 @@
 #pragma once
 
+/* Abstract Tower Class. Defines Functionality for classes
+* implementing Tower.
+*/
+
+#include <string>
+#include <sstream>
 #include "Entity.h"
 
-class Tower : public Entity{
+class Tower: public Entity{
 public:
-	Tower();
+	virtual int getX() = 0;
+	virtual int getY() = 0;
+	virtual int getLevel() = 0;
+	virtual int getDamage() = 0;
+	virtual int getRange() = 0;
+	virtual int getFireRate() = 0;
+	virtual int getBasePrice() = 0;
+	virtual int getSellPrice() = 0;
+	virtual int getUpgradePrice() = 0;
+	std::string to_string(){
+		std::stringstream ss;
+		ss << "Tower:\n---------------------------------------" << std::endl;
+		ss << "Level:\t\t\t" << getLevel() << std::endl;
+		ss << "Base Price:\t\t" << getBasePrice() << std::endl;
+		ss << "Sell Price:\t\t" << getSellPrice() << std::endl;
+		ss << "Upgrade Price:\t\t" << getUpgradePrice() << std::endl;
+		ss << "Damage:\t\t\t" << getDamage() << std::endl;
+		ss << "Range:\t\t\t" << getRange() << std::endl;
+		ss << "Fire Rate:\t\t" << getFireRate() << std::endl;
+		std::string str = ss.str();
+		return str;
+	}
 };
